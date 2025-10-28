c575951dae4e5c71aa13a9f69377afda0fa43b13
Pedro Alejandro Lima Dos Anjos <alejandropedro548@gmail.com>
2025-10-28 16:42:18 -0300

    Add files via upload

---
+++ Repositorio_Base_Alunos-main/Modulo 2/Desenvolvimento Web/app.py
@@ -0,0 +1,44 @@
+
+## IMPORTANDO "PARTES" DO FLASK
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+
+nome = "Imune ao conhecimento"
+
+### Criando Rotas
+
+## / Significa principal onde o site já vai de cara
+
+@app.route("/")
+def ola():
+    return f"Olá {nome}"
+
+@app.route("/tchau")
+def tchau():
+    return f"Tchau {nome}"
+
+
+@app.route("/adeus")
+def adeus():
+    return f"Faltam 8 dias para o professor se aposentar 🤣😎🎉" 
+
+## Aqui vamos criar uma nova rota, dessa vez vai ser a /contato
+
+@app.route("/contato")
+def contato():
+    return render_template('contato.html')
+
+
+## Sua vez crie uma nova rota dessa vez /hobbies, la coloque algo que você goste de fazer, exemplo : Jogar bola
+
+# lembre- se de criar um template chamado hobbies.html
+@app.route("/hobbies")
+def hobbies():
+    meus_hobbies = ['Jogar Bola', 'Dançar', 'Pescar','Programar']
+    return render_template('hobbies.html', hobbies = meus_hobbies)
+
+## Executando o arquivo 
+if __name__ == '__main__':
+    app.run(debug=True)
