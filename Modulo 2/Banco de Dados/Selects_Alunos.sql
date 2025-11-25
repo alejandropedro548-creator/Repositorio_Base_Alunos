cbbf0b0463911af170e9792403f157a38d783848
Pedro Alejandro Lima Dos Anjos <alejandropedro548@gmail.com>
2025-11-25 16:40:52 -0300

    Add files via upload

---
+++ Modulo 2/Banco de Dados/Selects_Alunos.sql
@@ -0,0 +1,34 @@
+select * FROM alunos
+INSERT INTO alunos (nome,idade) VALUES("Pedro Alejandro",17)
+INSERT INTO alunos (nome,idade) VALUES("Cristian",15)
+INSERT INTO alunos (nome,idade) VALUES("Arthur",16)
+INSERT INTO alunos (nome,idade) VALUES("Guilherme",16)
+INSERT INTO alunos (nome,idade) VALUES("João",15)
+INSERT INTO alunos (nome,idade) VALUES("Pai de familia",15)
+INSERT INTO alunos (nome,idade) VALUES("Nicolas",17)
+INSERT INTO alunos (nome,idade) VALUES("Wender",16)
+INSERT INTO alunos (nome,idade) VALUES("Arthur",15)
+INSERT INTO alunos (nome,idade) VALUES("Emanuelle",18)
+INSERT INTO alunos (nome,idade) VALUES("André",16)
+INSERT INTO alunos (nome,idade) VALUES("Andressa",16)
+INSERT INTO alunos (nome,idade) VALUES("Yasmin",17)
+INSERT INTO alunos (nome,idade) VALUES("Thauany",16)
+INSERT INTO alunos (nome,idade) VALUES("Lucas",17)
+INSERT INTO alunos (nome,idade) VALUES("Henrique",16)
+INSERT INTO alunos (nome,idade) VALUES("Breno",16)
+INSERT INTO alunos (nome,idade) VALUES("Isaias",17)
+INSERT INTO alunos (nome,idade) VALUES("Eshiley",16)
+INSERT INTO alunos (nome,idade) VALUES("cauane",17)
+INSERT INTO alunos (nome,idade) VALUES("Talita",15)
+INSERT INTO alunos (nome,idade) VALUES("Lima",15)
+INSERT INTO alunos (nome,idade) VALUES("Yasmin",15)
+INSERT INTO alunos (nome,idade) VALUES("Leonardo",16)
+INSERT INTO alunos (nome,idade) VALUES("Rodrigo",15)
+INSERT INTO alunos (nome,idade) VALUES("Ryan",15)
+SELECT * FROM alunos WHERE idade >= 17
+
+SELECT * FROM alunos ORDER by matricula
+
+SELECT * FROM alunos WHERE nome = Yasmin
+
+select * FROM alunos WHERE nome like "%Yasmin%"
